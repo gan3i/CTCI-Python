7ede31fb8719768cd2b47f7339e1090cf0cff297
Gan3i <gani468@gmail.com>
2020-12-29 11:43:33 +0530

    dfs search added

--- ArraysAndStrings.py/TreeAndGraph/Route_between_nodes_4.1.py
+++ ArraysAndStrings.py/TreeAndGraph/Route_between_nodes_4.1.py
@@ -10,39 +10,55 @@
 #Assumptions are
 # it's sparse and connected graph, just have to return whetheer a path exists or not
 # inputs are valid
+
 from queue import Queue
-def search(graph, start, end):
+from collections import defaultdict
+from typing import DefaultDict, List
 
-    if start == end:
-        return True
-
-    visited = set()
+def bfs_search(graph: DefaultDict ,start : str,end : str) -> bool:
+    visited_nodes = set()
     q = Queue()
     q.put(start)
-
     while not q.empty():
-
-        curr = q.get()
-        visited.add(curr)
-        
-        for adj in graph[curr]:
-            if adj not in visited:
-                if adj == end:
-                    return True
-                else:
-                    q.put(adj)
+        curr_node = q.get()
+        if curr_node == end:
+            return True
+        else:
+            visited_nodes.add(curr_node)
+        for adj_node in graph[curr_node]:
+            if adj_node not in visited_nodes:
+                q.put(adj_node)
 
     return False
 
+from queue import Queue
 from collections import defaultdict
+from typing import DefaultDict, List
+
+def search(graph : DefaultDict, start : str, end: str) ->bool:
+
+    def dfs_search(graph,curr_node,end,visited):
+
+        if curr_node == end:
+            return True
+        else:
+            for adj_node in graph[curr_node]:
+                if adj_node not in visited:
+                    visited.add(curr_node)
+                    return True if  dfs_search(graph,adj_node, end,visited) else False
+
+    return dfs_search(graph, start, end, set(start))
+
 g = defaultdict(set)
 
-g[1] = {2,3,4}
-g[2] = {5,3}
-g[3] = {4,1}
+g['A'] = {'B'}
+g['B'] = {'D'}
+g['C'] = {'A','D'}
+g['D'] = {'E'}
+g['E'] = {'F'}
 
 
-print(search(g, 3,5))
 
-
+print(search(g, 'A','C'))
+print(bfs_search(g, 'B','C'))
 
