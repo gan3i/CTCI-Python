e181e0abd5da66011ec9d55b036ca18584ba7b69
Gan3i <gani468@gmail.com>
2021-01-04 13:39:01 +0530

    o(h) solution for successor problem

--- TreeAndGraph/successor_4.6.py
+++ TreeAndGraph/successor_4.6.py
@@ -9,7 +9,6 @@ class BinaryTreeNode:
         self.parent = parent
 
 
-
 def successor(node: BinaryTreeNode) -> Optional[BinaryTreeNode]:
     if not node:
         return None
@@ -22,22 +21,37 @@ def successor(node: BinaryTreeNode) -> Optional[BinaryTreeNode]:
     return None
 
 
-
 def fill_sorted_items(root: BinaryTreeNode, sorted_items: List[BinaryTreeNode]) -> None:
     if not root:
-        return 
+        return
     fill_sorted_items(root.left, sorted_items)
     sorted_items.append(root)
     fill_sorted_items(root.right, sorted_items)
-
 
 
 def get_root(node: BinaryTreeNode) -> BinaryTreeNode:
     if not node.parent:
         return node
     return get_root(node.parent)
-    
 
+
+def successor1(node: BinaryTreeNode) -> Optional[BinaryTreeNode]:
+    if not node:
+        return None
+    if node.right:
+        return left_most_child(node.right)
+    curr : BinaryTreeNode= node
+    parent : Optional[BinaryTreeNode] = curr.parent
+    while parent and parent.left != curr:
+        curr = parent
+        parent = parent.parent
+    return parent
+
+
+def left_most_child(node: BinaryTreeNode) -> Optional[BinaryTreeNode]:
+    while node and node.left:
+        node = node.left
+    return node
 
 root = BinaryTreeNode(6, None)
 root.left = BinaryTreeNode(3, root)
@@ -47,10 +61,9 @@ root.left.right = BinaryTreeNode(4, root.left)
 root.right.left = BinaryTreeNode(7, root.right)
 root.right.right = BinaryTreeNode(10, root.right)
 
- #         6
- #      /      \
- #    3         9
- #  /   \      /  \
- # 1     4    7    10
-print(successor(root.right.left).data)
-
+#         6
+#      /      \
+#    3         9
+#  /   \      /  \
+# 1     4    7    10
+print(successor1(root.right.left).data)
