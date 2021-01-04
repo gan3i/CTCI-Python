3c32fdcfd98c6173e35237c4506daaf27f434e2b
Gan3i <gani468@gmail.com>
2021-01-04 10:30:13 +0530

    formated with balck

--- TreeAndGraph/validate_bst_4.5.py
+++ TreeAndGraph/validate_bst_4.5.py
@@ -8,21 +8,22 @@ def is_bst(root: BinaryTreeNode) -> bool:
         fill_sorted_items(root.left)
         sorted_items.append(root.data)
         fill_sorted_items(root.right)
+
     sorted_items = []
     fill_sorted_items(root)
     i = 1
     while i < len(sorted_items):
-        if sorted_items[i-1] >= sorted_items[i]:
+        if sorted_items[i - 1] >= sorted_items[i]:
             return False
-        i +=1
+        i += 1
 
     return True
-
 
 
 class WrapPreviousNodeData:
     def __init__(self, data):
         self.data = data
+
 
 def is_bst1(root: BinaryTreeNode) -> bool:
     def validate_bst(root):
@@ -36,23 +37,25 @@ def is_bst1(root: BinaryTreeNode) -> bool:
         if not validate_bst(root.right):
             return False
         return True
-    
+
     previous_node_data: WrapPreviousNodeData = WrapPreviousNodeData(float("-inf"))
     return validate_bst(root)
 
 
-    
 def is_bst2(root: BinaryTreeNode) -> bool:
     def validate_bst(root, min, max):
         if not root:
             return True
         if root.data <= min or root.data >= max:
             return False
-        if not (validate_bst(root.left, min, root.data) and validate_bst(root.right, root.data, max)):
+        if not (
+            validate_bst(root.left, min, root.data)
+            and validate_bst(root.right, root.data, max)
+        ):
             return False
         return True
-    return validate_bst(root, float("-inf"), float("+inf"))
 
+    return validate_bst(root, float("-inf"), float("+inf"))
 
 
 root = BinaryTreeNode(6)
@@ -61,7 +64,7 @@ root.right = BinaryTreeNode(9)
 root.left.left = BinaryTreeNode(1)
 root.left.right = BinaryTreeNode(8)
 root.right.left = BinaryTreeNode(7)
-root.right.right= BinaryTreeNode(10)
+root.right.right = BinaryTreeNode(10)
 
 
 print(is_bst1(root))
